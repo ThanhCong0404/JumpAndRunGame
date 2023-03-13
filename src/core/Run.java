275e244715a89193012df2328044a98b31a2efa4
ThanhCong0404 <Thanhcong04042001@gmail.com>
2023-03-13 22:49:18 +0700

    Init project Jump And Run Game

---
+++ src/core/Run.java
@@ -0,0 +1,9 @@
+package core;
+
+public class Run {
+    public static void main(String[] args) {
+        //Start project (MAIN PROJECT)
+        new Window("Thanh Cong");
+
+    }
+}
---
+++ src/core/Window.java
@@ -0,0 +1,24 @@
+package core;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Window extends Canvas implements Runnable {
+
+    public Window(String title){
+        JFrame frame = new JFrame(title);
+
+        frame.setSize(800,600);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setResizable(true);
+        frame.setVisible(true);
+        frame.add(this);
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
