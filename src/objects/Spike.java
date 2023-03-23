150bbdc43e85559845c0362592ba3451221052d5
ThanhCong0404 <Thanhcong04042001@gmail.com>
2023-03-23 23:27:49 +0700

    handle Sike collisions and Respawn Mechanic (co che hoi sinh khi va cham)

--- src/objects/Spike.java
+++ src/objects/Spike.java
@@ -6,8 +6,21 @@ public class Spike extends Item{
     public int x,y,width,height;
     public Color c;
 
+    public int[] PolyX = new int[3],PolyY = new int[3];
+    public Polygon p; //đa giác
+
     public Spike(byte id, int x, int y, int width, int height, Color c) {
         super(id);
+        PolyX[0] = x;
+        PolyX[1] = x+width/2;
+        PolyX[2] = x+width;
+
+        PolyY[0] = y+height;
+        PolyY[1] = y;
+        PolyY[2] = y+height;
+
+        p = new Polygon(PolyX,PolyY,3);
+
         this.x = x;
         this.y = y;
         this.width = width;
@@ -17,7 +30,7 @@ public class Spike extends Item{
     @Override
     public void render(Graphics g) {
         g.setColor(c);
-        g.fillOval(x,y,width,height);
+        g.drawPolygon(p);
     }
 
     @Override
